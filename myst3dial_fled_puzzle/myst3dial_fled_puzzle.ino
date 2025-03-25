506a5bc6b8a67573a84a79c9a195585a7089c7ae
consciousvisionaries <paul@consciousvisionaries.ca>
2025-03-25 10:34:20 -0700

    Update myst3dial_fled_puzzle.ino

--- myst3dial_fled_puzzle/myst3dial_fled_puzzle.ino
+++ myst3dial_fled_puzzle/myst3dial_fled_puzzle.ino
@@ -1,22 +1,28 @@
-
+*!
 
 // access point
 #define AP_SSID "LOST 3 Dial Puzzle"
 #define AP_PASSWORD "MystTech" // 'localhost:1880/admin'
 
-#define PUZZLE_SOLUTION { 1, 3, 5, 7, 9, 11, 13, 15 }
+#define PUZZLE_SOLUTION { 16, 12, 15 }
 #define NUM_OF_SOLUTIONS 3
 #define RECORD_LOG true
 #define SHOW_LOG true
 #define SIZE_LOG 250
 #define RESET_PREF false
 
-#define EXEC_BATCH1_PIN 27
-#define EXEC_BATCH2_PIN 14
+
+
+int EXEC_BATCH1_PIN = 19;
+int EXEC_BATCH2_PIN = 18;
+
+#define GPIO12_ADC2_CH5_HSPI_MOS0_TOUCH5_MTDI 12
+#define GPIO16_U2RXD_WS2812_16 16
+
 #define FLED_PIN1 12
 #define FLED_PIN2 16
 
-#define PUZZLE_NAME "LOST 3 Dial Puzzle"
+#define PUZZLE_NAME "LOST 3 Poison Puzzle"
 #define DESIGNER_NAME "Beyond Belief"
 #define TECH_NAME "MYSTTECH Creations"
 #define MYSTTECH_MODEL "MYST3DIAL_FLED_PUZZLE"
@@ -25,10 +31,11 @@
 #define NR_TAB "LOST"
 #define NR_GROUP "Stage 1"
 #define NR_TYPE "3D_ROTARY_PULSE"
-#define PULSE_MAX_RANGE 20 // NR_TYPE "3D_ROTARY_PULSE"
+int PULSE_MAX_RANGE[3] = { 20000, 20000, 2000 }; // NR_TYPE "3D_ROTARY_PULSE"
 
 #define NUM_DIGITAL_INPUTSA 2
 #define NUM_DIGITAL_INPUTSB 0
+#define NUM_DIGITAL_IOMATRIXPAIRS 0
 #define NUM_ANALOG_INPUTPAIRS 3
 #define NUM_DIGITAL_OUTPUTSA 2
 #define NUM_DIGITAL_OUTPUT_ROWSA 1
@@ -36,11 +43,13 @@
 #define NUM_DIGITAL_OUTPUT_ROWSB 0
 #define NUM_FLED_OUTPUTS 2
 #define NUM_FLED_CHANNELS 3  // 30/3
-#define NUM_FLED_ADDLEDS 90 // Total number of LEDs (10 per dial)
+#define NUM_FLED_ADDLEDS (18 * 3) // Total number of LEDs (10 per dial)
 #define NUM_FLED_ADDLEDS2 10 // total for around the room.
 #define NUM_RXTX_PORTS 0
+#define NUM_I2C_PORTS 0
 
-String bodyInputTitle = "3 Dials MP3 Puzzle";
+
+String bodyInputTitle = "3 Poisons FLED Puzzle";
 String bodyOutputATitle = "Override";
 String bodyOutputBTitle = "";
 String buttonsHTMLTitle = "";
@@ -48,7 +57,7 @@ String buttonsHTMLTitle = "";
 // github
 const char* GITHUB_USER = "consciousvisionaries";
 const char* GITHUB_REPO = "LOST_MystTech";
-const char* GITHUB_BRANCH = "LOST_4X4MATRIXMP3/myst3dial_fled_puzzle";
+const char* GITHUB_BRANCH = "LOST_POISON_3DIALFLED/myst3dial_fled_puzzle";
 const char* GITHUB_BIN = "myst3dial_fled_puzzle.ino.esp32da.bin"; // esp32 wroom
 
 
@@ -87,7 +96,7 @@ struct MQTTSettings {
   String mqttPassword = "8bdehprsuz";
   String mqttBroker = "b37.mqtt.one";
   const char* mqttTopic = "9grsvy8373/lost";
-  String bup_mqttBroker[3] = {"b37.mqtt.one", "192.168.0.129", "broker.emqx.io"};
+  String bup_mqttBroker[3] = {"b37.mqtt.one", "127.0.0.1", "broker.emqx.io"};
   String bup_mqttUser[3] = {"9grsvy8373", "pro1polaris", ""};
   String bup_mqttPassword[3] = {"8bdehprsuz", "CVr819P*!", ""};
 
@@ -99,7 +108,7 @@ struct GlobalSettings {
 
   String inputNames[8] = { "Dial 1a", "Dial 1b", "Dial 2a", "Dial 2b", "Dial 3a", "Dial 3b", "not used", "not used" };
   String outputNames[8] = { "Dial 1a", "Dial 1b", "Dial 2a", "Dial 2b", "Dial 3a", "Dial 3b", "not used", "not used" };
-  String colorsFLEDChannels[3] = {"red", "green", "blue"};
+  String colorsFLEDChannels[3] = {"red ", "green", "blue"};
 
 };
 
